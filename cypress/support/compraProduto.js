42f33124bc42de974ff1c7c0d4a34f26c66f2960
Victor Hugo <victorhugo.lemobs@gmail.com>
2024-02-26 11:38:50 -0300

    Final

--- cypress/support/compraProduto.js
+++ cypress/support/compraProduto.js
@@ -11,17 +11,20 @@ class compraProduto {
 
     finalizar_compra() {
         cy.visit('/')
+        cy.contains('span','1').click
         cy.get('.showcart').click()
-       // cy.contains('span','My Cart').click()
-        cy.get('#top-cart-btn-checkout').click()
-        cy.get('#C5HDOK8').type('Teste')
-        cy.get('#XCF5UCC').type('6146 Honey Bluff Parkway')
-        cy.get('#OVIP4WK').click().type('Mic').type('{enter}')
-        cy.get('#P051EJF').type('Calder')
-        cy.get('#T774UQD').type('49628-7978')
-        cy.get('#DOU66JS').type('(555) 229-3326')
+        cy.wait(5000)
+        cy.contains('span','Proceed to Checkout').click()
+        cy.wait(6000)
+        cy.get('[name="company"]').type('Teste')
+        cy.get('[name="street[0]"]').type('6146 Honey Bluff Parkway')
+        cy.get('[name="region_id"]').select('Michigan')
+        cy.get('[name="city"]').type('Calder')
+        cy.get('[name="postcode"]').type('49628-7978')
+        cy.get('[name="telephone"]').type('(555) 229-3326')
         cy.contains('td','Table Rate').click()
         cy.contains('span','Next').click()
+        cy.wait(4000)
         cy.contains('span','Place Order').click()
         cy.contains('span','Thank you for your purchase!').should('be.visible')
     }
